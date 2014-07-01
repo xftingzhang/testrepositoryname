51be105429d5592f44ffb036569794fb7a65d893
zhangting <13123900015@163.com>
2014-07-01 14:35:26 +0800

    Change-Id: I92623a2cd89d3d8209c10bf47348fb0f002f5a6a

---
+++ src/main/java/mygroupid/artifactid/Sabc.java
@@ -0,0 +1,14 @@
+package mygroupid.artifactid;
+
+public class Sabc {
+
+  public Sabc() {
+    // TODO Auto-generated constructor stub
+  }
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
