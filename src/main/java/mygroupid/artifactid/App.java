7e7f11506512506811bd1804c4b00f917558d199
zhangting <13123900015@163.com>
2014-07-01 14:19:57 +0800

    for branch
    Change-Id: I3a8879db70c203a3d8cb6b829c5a5ff0ea985fa6

--- src/main/java/mygroupid/artifactid/App.java
+++ src/main/java/mygroupid/artifactid/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!7890uiui" );
-        String abc = "";
+        String abc = "mybranch";
     }
 }
